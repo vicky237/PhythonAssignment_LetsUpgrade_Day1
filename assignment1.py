82610fda4ad45aa44d9e1b547a46ae9a5d75ba09
VIKNESH V <40331699+vicky237@users.noreply.github.com>
2021-01-05 23:57:03 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+print('hai hello')
+username = input("Enter user name:")
+password = input("Enter Passsword : ")
+
+count = 1
+while count <  6
+if (username == 'INDIA' ):
+	if(password == 'IND'):
+		print("Success")
+	break
+else:
+ print("Enter correct Pasword")
+
+else:
+ print("Enter correct user name")
